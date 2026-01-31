10949e05ec1042b25330efa2ca0886e53e79f025
safayet404 <hossainsafayet187@gmail.com>
2026-02-01 00:55:59 +0600

    data show with pagination and custom field

--- src/components/Admin Dashboard/StudentsList.vue
+++ src/components/Admin Dashboard/StudentsList.vue
@@ -4,17 +4,18 @@ import { onMounted, ref, watch } from 'vue';
 
 import { useStudentStore } from '@/stores/student';
 import { storeToRefs } from 'pinia';
+import { Check, CheckCheck, Cross, Eye, X } from 'lucide-vue-next';
+import { CanceledError } from 'axios';
 
 const store = useStudentStore()
 const { students, pagination, isFetching } = storeToRefs(store)
 const { fetchStudents } = store
-console.log("state", students.value);
-console.log("pagination", pagination.value);
-
 const headers = [
     { text: 'ID', value: 'id', sortable: true },
     { text: 'NAME', value: 'full_name', sortable: true },
-    { text: 'PERMISSIONS', value: 'permissions' }, // We will use a slot for this
+    { text: 'email', value: 'email' },
+    { text: 'INSTITUTION', value: 'institution' },
+    { text: 'Profile', value: 'profile' },
     { text: 'ACTIONS', value: 'operation', width: 250 },
 ];
 
@@ -24,12 +25,10 @@ const serverOptions = ref({
 });
 
 watch(serverOptions, (value) => {
-    // When the user changes page OR changes "rows per page", fetch again
     fetchStudents(value.page, value.rowsPerPage);
 }, { deep: true });
 
 onMounted(async () => {
-    // Initial load using the default server options
     await fetchStudents(serverOptions.value.page, serverOptions.value.rowsPerPage);
 });
 </script>
@@ -39,21 +38,44 @@ onMounted(async () => {
 
         <Head title="Student Records" />
 
-        <EasyDataTable v-model:server-options="serverOptions" :server-items-length="pagination.total"
-            :loading="isFetching" :headers="headers" :items="students" buttons-pagination
-            table-class-name="customize-table" class="rounded-lg shadow-md">
-            <template #item-institution="{ profiles }">
-                {{ profiles[0]?.institution || 'N/A' }}
-            </template>
+        <div class="p-4 ">
+            <EasyDataTable v-model:server-options="serverOptions" :server-items-length="pagination.total"
+                :loading="isFetching" :headers="headers" :items="students" buttons-pagination
+                table-class-name="customize-table" class="rounded-lg shadow-md">
+                <template #item-institution="{ profiles }">
+                    {{ profiles[0]?.institution || 'N/A' }}
+                </template>
+                <template #item-profile="{ profiles }">
 
-            <template #item-operation="item">
-                <div class="flex items-center gap-2 py-2">
-                    <Link :href="`/students/${item.id}`"
-                        class="rounded bg-blue-600 px-3 py-1 text-xs text-white hover:bg-blue-700">
-                    Show
-                    </Link>
-                </div>
-            </template>
-        </EasyDataTable>
+                    <CheckCheck v-if="profiles[0]?.profile_id" class="text-green-500 font-bold" />
+
+                    <X v-else class="w-5 h-5 text-red-700 font-bold" />
+                </template>
+
+                <template #item-operation="{ profiles }">
+
+                    <Eye />
+
+                </template>
+
+            </EasyDataTable>
+
+        </div>
     </div>
 </template>
+
+<style>
+.customize-table {
+    --easy-table-header-background-color: #8176f1;
+    --easy-table-header-font-color: #fff;
+    --easy-table-header-height: 70px;
+    --easy-table-header-font-size: 15px;
+    --easy-table-body: 15px;
+    --easy-table-body-row-height: 76px;
+    --easy-table-footer-font-color: #7367F0;
+    --easy-table-scrollbar-track-color: #2d3a4f;
+    --easy-table-scrollbar-color: #2d3a4f;
+    --easy-table-scrollbar-thumb-color: #4c5d7a;
+    --easy-table-scrollbar-corner-color: #2d3a4f;
+}
+</style>
